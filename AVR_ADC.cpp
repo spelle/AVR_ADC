a63cd645bdd37ee5c210c07bab7f7a65343fae47
spelle <pelle.sebastien@gmail.com>
2013-10-11 13:39:30 +0200

    Eclipse Project file Source

--- AVR_ADC.cpp
+++ AVR_ADC.cpp
@@ -17,12 +17,6 @@
 
 
 
-#define FOSC F_CPU // Clock Speed
-#define BAUD 9600
-#define MYUBRR FOSC/16/BAUD-1
-
-
-
 ISR(TIMER1_COMPA_vect)
 {
 	// Toggle the LED
@@ -39,16 +33,16 @@ ISR(TIMER1_COMPA_vect)
 int main( void )
 {
 	// Set Port B direction Register
-	DDRB |= (1<<DDB5) ; // For LED on Pin PORTB5
+	DDRB 	|= (1<<DDB5) ; // For LED on Pin PORTB5
 
 	// Configure the Timer/Counter1 Control Register A
-	TCCR1A = 0 ;
+	TCCR1A	= 0 ;
 	// Configure the Timer/Counter1 Control Register B
-	TCCR1B = (1 << WGM12) ; // Configure timer 1 for CTC mode
+	TCCR1B	= (1 << WGM12) ; // Configure timer 1 for CTC mode
 
-	TIMSK1 = (1 << OCIE1A); // Enable CTC interrupt
+	TIMSK1	= (1 << OCIE1A); // Enable CTC interrupt
 
-	OCR1A   = 15624 ; // Set CTC compare value to 1Hz at 1MHz AVR clock, with a prescaler of 64
+	OCR1A	= 15624 ; // Set CTC compare value to 1Hz at 1MHz AVR clock, with a prescaler of 64
 
 	sei(); //  Enable global interrupts
 
@@ -56,10 +50,8 @@ int main( void )
 
 	// Configure ADC
 	ADMUX |= (1 << REFS0) | (1 << MUX3) ; // Select ADC8
-
 	// Enable ADC, Enable Interrupts, Select Prescaler to CLK/128
 	ADCSRA |= (1 << ADEN) | (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0) ;
-
 
 
 	TCCR1B |= (1 << CS12) ; // Start timer at Fcpu/64
