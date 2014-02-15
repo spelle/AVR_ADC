18886b86a267ef48e090b3ef8a9421dcaebde287
spelle <pelle.sebastien@gmail.com>
2014-02-15 13:18:37 +0100

    Lookup-Table

--- AVR_ADC.c
+++ AVR_ADC.c
@@ -11,15 +11,6 @@
 
 
 
-#define B 13349 // 208.9 in FIXED(6)
-#define A 774 // 12.1 in FIXED(6)
-
-
-
-uint16_t uiCurrentADCChannel = 0 ;
-
-
-
 ISR(TIMER1_COMPA_vect)
 {
 	// Toggle the LED
@@ -28,53 +19,55 @@ ISR(TIMER1_COMPA_vect)
 	// Read the ADC value
 	uint16_t uiADCvalue = ADC_Read() ;
 
-	if ( 0 == uiCurrentADCChannel )
-	{
-		double dA, dB, dB1, dB2, dY1, dY2, dX1, dX2 ;
-		if( uiADCvalue < Temp_vs_ADC_Value_LookupTable[0].uiADC_Value )
-		{
-			dX1 = Temp_vs_ADC_Value_LookupTable[0].uiTemp ;
-			dY1 = Temp_vs_ADC_Value_LookupTable[0].uiADC_Value ;
-			dX2 = Temp_vs_ADC_Value_LookupTable[1].uiTemp ;
-			dY2 = Temp_vs_ADC_Value_LookupTable[1].uiADC_Value ;
-		}
-		else if( uiADCvalue >= Temp_vs_ADC_Value_LookupTable[sizeof(Temp_vs_ADC_Value_LookupTable)/sizeof(Temp_vs_ADC_Value_t)-1].uiADC_Value )
-		{
-			dX1 = Temp_vs_ADC_Value_LookupTable[sizeof(Temp_vs_ADC_Value_LookupTable)/sizeof(Temp_vs_ADC_Value_t)-2].uiTemp ;
-			dY1 = Temp_vs_ADC_Value_LookupTable[sizeof(Temp_vs_ADC_Value_LookupTable)/sizeof(Temp_vs_ADC_Value_t)-2].uiADC_Value ;
-			dX2 = Temp_vs_ADC_Value_LookupTable[sizeof(Temp_vs_ADC_Value_LookupTable)/sizeof(Temp_vs_ADC_Value_t)-1].uiTemp ;
-			dY2 = Temp_vs_ADC_Value_LookupTable[sizeof(Temp_vs_ADC_Value_LookupTable)/sizeof(Temp_vs_ADC_Value_t)-1].uiADC_Value ;
-		}
-		else
-		{
-			uint16_t ui ;
-			for(
-				ui = 0 ;
-				ui < sizeof(Temp_vs_ADC_Value_LookupTable)/sizeof(uint16_t) &&
-				uiADCvalue >= Temp_vs_ADC_Value_LookupTable[ui].uiADC_Value ;
-				ui++ ) ;
+	double dA, dB, dB1, dB2, dY1, dY2, dX1, dX2 ;
 
-			dX1 = (ui*5.0-55.0) ;
-			dY1 = Temp_vs_ADC_Value_LookupTable[ui].uiADC_Value ;
-			dX2 = ((ui+1)*5.0-55.0) ;
-			dY2 = Temp_vs_ADC_Value_LookupTable[ui+1].uiADC_Value ;
-		}
+	if( uiADCvalue < Temp_vs_ADC_Value_LookupTable[0].uiADC_Value )
+	{	// Acquired ADC Value is lower than lower bound
+		// Takes the two first points
+		dX1 = Temp_vs_ADC_Value_LookupTable[0].uiTemp ;
+		dY1 = Temp_vs_ADC_Value_LookupTable[0].uiADC_Value ;
+		dX2 = Temp_vs_ADC_Value_LookupTable[1].uiTemp ;
+		dY2 = Temp_vs_ADC_Value_LookupTable[1].uiADC_Value ;
+	}
+	else if( uiADCvalue >= Temp_vs_ADC_Value_LookupTable[sizeof(Temp_vs_ADC_Value_LookupTable)/sizeof(Temp_vs_ADC_Value_t)-1].uiADC_Value )
+	{	// Acquired ADC Value is greater than upper bound
+		// Takes the two last points
+		dX1 = Temp_vs_ADC_Value_LookupTable[sizeof(Temp_vs_ADC_Value_LookupTable)/sizeof(Temp_vs_ADC_Value_t)-2].uiTemp ;
+		dY1 = Temp_vs_ADC_Value_LookupTable[sizeof(Temp_vs_ADC_Value_LookupTable)/sizeof(Temp_vs_ADC_Value_t)-2].uiADC_Value ;
+		dX2 = Temp_vs_ADC_Value_LookupTable[sizeof(Temp_vs_ADC_Value_LookupTable)/sizeof(Temp_vs_ADC_Value_t)-1].uiTemp ;
+		dY2 = Temp_vs_ADC_Value_LookupTable[sizeof(Temp_vs_ADC_Value_LookupTable)/sizeof(Temp_vs_ADC_Value_t)-1].uiADC_Value ;
+	}
+	else
+	{	// Find the interval where to perfom the computing of temperature
+		uint16_t ui ;
+		for(
+			ui = 0 ;
+			ui < sizeof(Temp_vs_ADC_Value_LookupTable)/sizeof(uint16_t) &&
+			uiADCvalue >= Temp_vs_ADC_Value_LookupTable[ui].uiADC_Value ;
+			ui++ ) ;
 
-		dA = ( dY2 - dY1)/( dX2 - dX1 ) ;
-		dB2 = dY2 - dA*dX2 ;
-		dB1 = dY1 - dA*dX1 ;
-		dB  = (dB1+dB2)/2.0 ;
-
-		double dTemp = (uiADCvalue - dB) / dA ;
-		dTemp -= 1.0 ;
-
-		PDEBUG( "CTN-Float\t%f\n", dTemp ) ;
-
-		//uiCurrentADCChannel = 0 ;
+		// Computes the X1, Y1, X2, Y2 points
+		dX1 = (ui*5.0-55.0) ;
+		dY1 = Temp_vs_ADC_Value_LookupTable[ui].uiADC_Value ;
+		dX2 = ((ui+1)*5.0-55.0) ;
+		dY2 = Temp_vs_ADC_Value_LookupTable[ui+1].uiADC_Value ;
 	}
 
+	// Computes the slope and offset fir the Lookup-segment
+	dA = ( dY2 - dY1)/( dX2 - dX1 ) ;
+	dB2 = dY2 - dA*dX2 ;
+	dB1 = dY1 - dA*dX1 ;
+	dB  = (dB1+dB2)/2.0 ;
+
+	// Computes the Temperature
+	double dTemp = (uiADCvalue - dB) / dA ;
+
+	//Removes 1.0 Celsius Degree Offset
+	dTemp -= 1.0 ;
+
+	PDEBUG( "CTN-Float\t%f\n", dTemp ) ;
+
 	// Restart a conversion
-	//ADC_SelectChannel( uiCurrentADCChannel ) ;
 	ADC_Start() ;
 }
 
@@ -120,7 +113,7 @@ int main( void )
 	ADC_SelectVoltageReference( AREF ) ;
 
 	// Select ADC0
-	ADC_SelectChannel( uiCurrentADCChannel ) ;
+	ADC_SelectChannel( 0 ) ;
 
 	// Enable the ADC, set prescaler to F_CPU/128
 	ADC_SelectPrescaler( FCPU_128 ) ;
