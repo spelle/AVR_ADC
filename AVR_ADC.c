ab1fc54c5feb1c79f78083255749c156c21a7955
Sébastien PELLÉ <pelle.sebastien@gmail.com>
2014-11-30 22:13:02 +0100

    Qualified some NTCs

--- AVR_ADC.c
+++ AVR_ADC.c
@@ -11,6 +11,14 @@
 
 
 
+#define MAX_ADC_CHANNEL 5
+
+
+
+char cCurrentADCChannel = 0 ;
+
+
+
 ISR(TIMER1_COMPA_vect)
 {
 	// Toggle the LED
@@ -53,7 +61,7 @@ ISR(TIMER1_COMPA_vect)
 		dY2 = Temp_vs_ADC_Value_LookupTable[ui+1].uiADC_Value ;
 	}
 
-	// Computes the slope and offset fir the Lookup-segment
+	// Computes the slope and offset for the Lookup-segment
 	dA = ( dY2 - dY1)/( dX2 - dX1 ) ;
 	dB2 = dY2 - dA*dX2 ;
 	dB1 = dY1 - dA*dX1 ;
@@ -62,58 +70,54 @@ ISR(TIMER1_COMPA_vect)
 	// Computes the Temperature
 	double dTemp = (uiADCvalue - dB) / dA ;
 
-	//Removes 1.0 Celsius Degree Offset
-	dTemp -= 1.0 ;
+	PDEBUG( "\t%f", dTemp ) ;
 
-	PDEBUG( "CTN-Float\t%f\n", dTemp ) ;
+	if( MAX_ADC_CHANNEL <= cCurrentADCChannel )
+	{
+		cCurrentADCChannel = 0 ;
+		PDEBUG( "\r\n\t" ) ;
+	}
+	else
+	{
+		cCurrentADCChannel++ ;
+	}
+
+	// Select the ADC Channel
+	ADC_SelectChannel( cCurrentADCChannel ) ;
 
 	// Restart a conversion
 	ADC_Start() ;
 }
 
 
-ISR(PCINT2_vect)
-{
-	if( 0 != (PIND & (1<<PIND6)) )
-	{
-		PORTD ^= (1<<PORTD5) ;
-	}
-}
-
 
 int main( void )
 {
 	// Set Port B Data Direction Register
 	DDRB	|= (1<<DDB5) ; // For LED on Pin PORTB5
-
 	// Set Port D Data Direction Register
 	DDRD	|= (1<<DDD5) ; // For LED on Pin PORTD5
-
-	// Set All mechanics for Port D6
-	PCMSK2	|= (1<<PCINT22) ;
-	PCICR	|= (1<<PCIE2) ;
-	PORTD	|= (1<<PORTD6) ; // Enable pullup resistor
 
 	// Configure the Timer/Counter1 Control Register A
 	TCCR1A	= 0 ;
 	// Configure the Timer/Counter1 Control Register B
 	TCCR1B	= (1 << WGM12) ; // Configure timer 1 for CTC mode
-
 	TIMSK1	= (1 << OCIE1A); // Enable CTC interrupt
-
 	OCR1A	= 62499 ; // Set CTC compare value to 1Hz at 1MHz AVR clock, with a prescaler of 64
-
 	sei(); //  Enable global interrupts
 
 	PDEBUG_INIT( 9600 ) ;
 
-	TCCR1B |= (1 << CS12) ; // Start timer at Fcpu/64
+	PDEBUG( "\nADC Channel # :" ) ;
+	for( char cCntr = 0 ; cCntr <= MAX_ADC_CHANNEL ; cCntr++ )
+		PDEBUG( "\t\t%i", cCntr ) ;
+	PDEBUG( "\r\n\t" ) ;
 
 	// Select the AVcc voltage reference
-	ADC_SelectVoltageReference( AREF ) ;
+	ADC_SelectVoltageReference( AVCC ) ;
 
-	// Select ADC0
-	ADC_SelectChannel( 0 ) ;
+	// Select the ADC Channel
+	ADC_SelectChannel( cCurrentADCChannel ) ;
 
 	// Enable the ADC, set prescaler to F_CPU/128
 	ADC_SelectPrescaler( FCPU_128 ) ;
@@ -122,6 +126,8 @@ int main( void )
 
 	// Start a Single Conversion
 	ADC_Start() ;
+
+	TCCR1B |= (1 << CS12) ; // Start timer at Fcpu/64
 
 	while( 1 )
 	{
